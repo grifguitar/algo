97756ba479bbe11a13a5ea598ddde1b3818e5357
Grigoriy Khlytin <xlootin@niuitmo.ru>
2022-03-16 12:02:51 +0300

    last commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# algo
+# Practical materials for the course of Algorithms, 2021
+
+## Course:
+
++ [Декартово дерево поиска](https://github.com/grifguitar/algo/tree/main/search_tree/)
++ [Строки: префикс и z-функция, хеши](https://github.com/grifguitar/algo/tree/main/strings/)
---
+++ search_tree/SearchTree.h
@@ -0,0 +1,19 @@
+//
+// Created by Grigoriy Khlytin on 15.12.2021.
+//
+
+#ifndef ITMO_CIRCLE_B_DIST_SEARCHTREE_H
+#define ITMO_CIRCLE_B_DIST_SEARCHTREE_H
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <string>
+#include <set>
+
+class SearchTree {
+public:
+    static bool solve();
+};
+
+#endif //ITMO_CIRCLE_B_DIST_SEARCHTREE_H
